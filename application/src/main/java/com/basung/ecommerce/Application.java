f52b354567d55543f3f6757878d13cbe953d503c
rocky.wang <rockyne@hotmail.com>
2018-10-19 09:43:33 +0800

    add wears models

--- application/src/main/java/com/basung/ecommerce/Application.java
+++ application/src/main/java/com/basung/ecommerce/Application.java
@@ -2,13 +2,11 @@ package com.basung.ecommerce;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 /**
  * Created with IntelliJ IDEA.
---
+++ ec-wares/src/main/java/com/basung/ecommerce/wears/test.java
@@ -0,0 +1,7 @@
+package com.basung.ecommerce.wears;
+
+/**
+ * Date: 2018-10-19-上午9:30
+ */
+public class test {
+}
